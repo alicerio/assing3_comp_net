5f70173493d3ee4a71eeea0de563aeda6515da79
alicerio <alicerio2@miners.utep.edu>
2022-03-19 14:12:39 -0600

    Update Server.py

--- Server/Server.py
+++ Server/Server.py
@@ -2,7 +2,7 @@
 File Transfer Assignment - Assignment 3
 CS 5313 - Computer Networks
 Last Modified By: Alan Licerio
-Last Modified on: March 18, 2022
+Last Modified on: March 19, 2022
 
 Program will run as following on the terminal:
 python3 Server.py
@@ -26,6 +26,13 @@ from udt import *
 # Create new socket
 serSock = socket(AF_INET, SOCK_STREAM)
 numPort = int(input('Listen at Port #: '))
+
+flag = True
+while flag:
+    method = input('Select GBN or SR: ')
+    if method == 'GBN' or method == 'SR':
+        flag = False
+
 try:
     # Bind to the given port
     serSock.bind(('127.0.0.1', numPort))
@@ -36,6 +43,56 @@ except:
 # Server ready to listen for connection
 serSock.listen(100)
 
+def gbn(conn, data, addr, file):
+    base = 0
+    next_seq = 0
+
+    # Have the user choose it perhaps
+    window_size = 4
+
+    while True:
+        print(next_seq)
+        # Make packet and send it if condition holds
+        packet = make(next_seq, data)
+        if next_seq < base + window_size:
+            send(packet, conn, addr)
+            next_seq+=1
+    # ACK packet received
+        ack_pkt, _ = recv(conn)
+        seq_num, ack_data = extract(ack_pkt)
+        if ack_data == b'ACK':
+            base = seq_num+1
+            if base == next_seq:
+                print
+                # stop time
+            else:
+                print
+                # end time
+        # if time out:
+            # send ALL packets
+        data = file.read(1020)
+        if not data:
+            break
+    print('Transfer Complete!')
+    print('Connection closed, See you later!')
+
+    # Shutdown to tell the client there is no more information to send.
+    conn.shutdown(2)
+    # conn.close()
+
+def sr(conn, data, addr, file):
+    base = 0
+    next_seq = 0
+    window_size = 4
+
+    while True:
+        for _ in range(window_size):
+            packet = make(next_seq, data)
+            if next_seq < base + window_size:
+                send(packet, conn, addr)
+                next_seq+=1
+
+
 while True:
     print('\nListening for conenction at ' + str(numPort))
     # Accept client connection
@@ -44,13 +101,6 @@ while True:
 
     # Boolean flag to determine if the file requested exists on the Server side
     fileExists = False
-
-    base = 0
-    next_seq = 0
-
-    # Have the user choose it perhaps
-    window_size = 4
-
     # File name received from the client
     data = conn.recv(1024).decode()
     print('Asking for file ' + data)
@@ -62,36 +112,13 @@ while True:
         
         fileExists = True
         print('Sending the file ...')
-        while data:
-            print(next_seq)
-            # Make packet and send it if condition holds
-            packet = make(next_seq, data)
-            if next_seq < base + window_size:
-                send(packet, conn, addr)
-                next_seq+=1
-
-            # ACK packet received
-            ack_pkt, a = recv(conn)
-            print(ack_pkt)
-            seq_num, ack_data = extract(ack_pkt)
-            if ack_data == b'ACK':
-                base = seq_num+1
-                if base == next_seq:
-                    print
-                    # stop time
-                else:
-                    print
-                    # end time
-            # if time out:
-                # send packets
-            data = file.read(1020)
-        print('Transfer Complete!')
-        print('Connection closed, See you later!')
     except IOError:
         # If file not exists, send an error and exit.
         if not fileExists:
             print('Error: ' + str(sys.exc_info()))
 
-    # Shutdown to tell the client there is no more information to send.
-    conn.shutdown(2)
-    # conn.close()
+    if method == 'GBN':
+        gbn(conn, data, addr, file)
+    elif method == 'SR':
+        sr(conn)
+
