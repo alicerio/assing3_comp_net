bd3034b0775ad0a55b69b05587bd8571f8c9dda5
joshuaramoscs <joshita1999@gmail.com>
2022-03-20 22:24:19 -0600

    GBN - server crashes after completion

--- Client/Client.py
+++ Client/Client.py
@@ -18,22 +18,19 @@ PROB
 *****
 """
 
-from socket import *
 import sys
-from packet import *
-from udt import *
+import socket
+import packet
+import udt
 
-# Get IP and Port # from user input
-# ip = raw_input('Provide server IP: ')
-numPort = int(input('Provide Port #: '))
-# Creates a new TCP socket
-cliSock = socket(AF_INET, SOCK_STREAM)
+# Creates a new socket
+cliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = int(input('Provide Port #: '))
+addr = ('127.0.0.1', port)
 try:
-    # Connects to the Server using the given values
-    cliSock.connect(('127.0.0.1', numPort))
+    cliSock.connect(('127.0.0.1', port))
     print('You are now connected! Enter your commands now.')
 except:
-    # Connection to the server was not successful
     print('Connection failed. Error: ' + str(sys.exc_info()))
     sys.exit(2)
 
@@ -41,7 +38,6 @@ except:
 RFTcommand = ''
 filename = ''
 next_seq = 0
-
 while True:
     # User input to retreive files. 
     RFTcommand = input('RFTCli> ')
@@ -49,36 +45,36 @@ while True:
     split = RFTcommand.split()
 
     # Flow of the program depending on the command entered
-    if (len(split) == 2 and split[0] != 'CLOSE'):
-        if(split[0] == "RETR"):
-            filename = split[1]
-            # Send filename requested to the server
-            cliSock.send(filename.encode())
-            # Open a new file with the same name
-            with open(filename, 'wb') as file:
-                while True:
-                    # Get packets from the server and write it to the file
-                    packet, addr = recv(cliSock)
-                    seq_num, data = extract(packet)
+    if (len(split) == 2 and split[0] == "RETR"):
+        filename = split[1]
+        # Send filename request to the server
+        first_pckt = packet.make(0, filename.encode())
+        udt.send(first_pckt, cliSock, addr)
 
-                    # Temp solution to get address
-                    addr = tuple(('127.0.0.1', numPort))
-                    if not data:
-                        break
-                    else:
+        # Open a new file with the same name
+        with open(filename, 'wb') as file:
+            expected_seq_num = 0
+            while True:
+                # Get packets from the server and write it to the file
+                pckt, s = udt.recv(cliSock)
+                seq_num, data = packet.extract(pckt)
+
+                if not data:
+                    print("Received last packet")
+                    break
+                else:
+                    print("Received packet #", seq_num)
+                    if seq_num == expected_seq_num:
                         file.write(data)
-                        # Create ACK message
-                        ack_data = b'ACK'
-                        ack_pkt = ''
-                        # Create ACK packet
-                        if seq_num == next_seq:
-                            ack_pkt = make(next_seq, ack_data)
-                            next_seq+=1
-                        else:
-                            ack_pkt = make(next_seq-1, ack_data)
-                        send(ack_pkt, cliSock, addr)
-
-            print('Received ' + filename)
+                        ack_pckt = packet.make(expected_seq_num, b'')  # Create ACK message
+                        udt.send(ack_pckt, cliSock, addr)
+                        expected_seq_num += 1
+                    else:
+                        print('Re-Sending ACK', expected_seq_num - 1)
+                        ack_pckt = packet.make(expected_seq_num - 1, b'')
+                        udt.send(ack_pckt, cliSock, addr)
+        file.close()
+        print('Received ' + filename)
     else:
         # Close socket connection
         cliSock.close()
--- Server/Server.py
+++ Server/Server.py
@@ -17,39 +17,97 @@ IT IS PEDNING TO INCORPORATE SLIDING WINDOWS, TIMER, AND LOST PACKET
 *****
 """
 
-from socket import *
 import sys
-from packet import *
-from udt import *
+import socket
+import threading
+import packet
+import udt
+import timer
+time = timer.Timer(1)
+base = 0
 
-# Create new socket and listen to port
-serSock = socket(AF_INET, SOCK_STREAM)
-numPort = int(input('Listen at Port #: '))
-try:
-    serSock.bind(('127.0.0.1', numPort))                 # Bind to port
-except:
-    print('Bind failed. Error: ' + str(sys.exc_info()))  # Bind failed
-    sys.exit(2)
-serSock.listen(100)                                      # Server ready to listen for connection
+def gbn_receiver(conn):
+    global time
+    global base
+    while True:
+        ack_packet, _ = udt.recv(conn)
+        ack, _ = packet.extract(ack_packet)
+        print('Received ACK #', ack)
+        if (ack >= base):
+            base = ack + 1
+            print('Base = ', base)
+            time.stop()
 
 
 # Function to call for a Go-Back-N pipeline protocol.
 # GBN resends entire window size when client/server sequences are inconsistent.
-def gbn(conn, addr, data, file):
-    base = 0         # The last acknowledged sequence number
-    seq_num = 0     # Current sequence number
+def gbn(conn, addr):
+    global time
+    global base      # The last acknowledged sequence number
+    seq_num = 0      # Current sequence number
     window_size = 4  # *Have the user choose it perhaps
+
+    filename_pckt, c = udt.recv(conn)
+    ack_seq_num, filename = packet.extract(filename_pckt)
+    filename = filename.decode()
+    print(filename)
+
+    try:  # Send if file exists
+        file = open(filename, 'rb')
+        packetsToSend = []  # list of  packets to be sent
+        recv_seq_num = 0  # received sequence number
+        while True:
+            data = file.read(1020)
+            if not data:
+                break
+            packetsToSend.append(packet.make(recv_seq_num, data))
+            recv_seq_num = recv_seq_num + 1
+
+        numberOfPackets = len(packetsToSend)  # number of packets that need to be sent
+        print("We have to send " + str(numberOfPackets) + " packets")
+        next_to_send = 0
+        recv_thread = threading.Thread(target=gbn_receiver, args=(conn,))
+        recv_thread.start()
+        while base < numberOfPackets:
+            # Send all the packets in the window
+            while next_to_send < base + window_size:
+                print('Sending packet', next_to_send)
+                udt.send(packetsToSend[next_to_send], conn, addr)
+                next_to_send = next_to_send + 1
+
+            # Start the timer
+            if not time.running():
+                print('Start Timer')
+                time.start()
+
+            # Wait until a timer goes off or we get an ACK
+            while time.running() and not time.timeout():
+                time.running()
+            if time.timeout():
+                print('Timeout')
+                time.stop()
+                next_to_send = base
+        udt.send(packet.make_empty(), conn, addr)
+        print("done sending file, have a good day :)")
+        file.close()
+
+
+    except IOError:  # If file does not exist, send error and exit.
+        print('Error: ' + str(sys.exc_info()))
+
 
     while True:
         print(seq_num)
         # Send packet
-        packet = make(seq_num, data)
+        pckt = packet.make(seq_num, data)
         if seq_num < base + window_size:
-            send(packet, conn, addr)
+            udt.send(pckt, conn, addr)
+            print("Sending #", seq_num)
             seq_num += 1
         # Receive acknowledgement
-        ack_pkt, _ = recv(conn)
-        ack_seq_num, ack_data = extract(ack_pkt)
+        ack_pkt, _ = udt.recv(conn)
+        ack_seq_num, ack_data = packet.extract(ack_pkt)
+        print("Received Ack #", ack_seq_num)
         if ack_data == b'ACK':
             base = ack_seq_num+1
             if base == seq_num:
@@ -65,10 +123,7 @@ def gbn(conn, addr, data, file):
             break
     print('Transfer Complete!')
     print('Connection closed, See you later!')
-
-    # Shutdown to tell the client there is no more information to send.
     conn.shutdown(2)
-    # conn.close()
 
 
 # Function to call for a Selective Repeat pipeline protocol.
@@ -80,10 +135,22 @@ def sr(conn, data, addr, file):
 
     while True:
         for _ in range(window_size):
-            packet = make(next_seq, data)
+            pckt = packet.make(next_seq, data)
             if next_seq < base + window_size:
-                send(packet, conn, addr)
+                udt.send(pckt, conn, addr)
                 next_seq+=1
+
+
+# Create new socket and listen to port
+serSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+port = int(input('Listen at Port #: '))
+try:
+    serSock.bind(('127.0.0.1', port))                    # Bind to port
+except:
+    print('Bind failed. Error: ' + str(sys.exc_info()))  # Bind failed
+    sys.exit(2)
+serSock.listen()                                         # Server ready to listen for connection
+print('\nListening for conenction at ' + str(port))
 
 
 # Select GBN or SR protocol
@@ -93,29 +160,18 @@ while flag:
     if method == 'GBN' or method == 'SR':
         print("Using ", method, "Protocol")
         flag = False
+
+
 # Listen for connections and send file
 while True:
-    # Accept client connection
-    print('\nListening for conenction at ' + str(numPort))
-    conn, addr = serSock.accept()
+    conn, addr = serSock.accept()       # Accept client connection
     print('Received a connection from:', addr)
 
-    # Determine if the file requested exists on the Server side
-    fileExists = False                  # True if file exists, false otherwise
-    data = conn.recv(1024).decode()     # File name received from the client
-    print('Checking for file: ' + data)
-    try:                                # Open file if it exists, send it to the client.
-        file = open(data, 'rb')
-        data = file.read(1020)
-        
-        fileExists = True
-        print('Sending the file ...')
-    except IOError:                     # If file does not exist, send error and exit.
-        if not fileExists:
-            print('Error: ' + str(sys.exc_info()))
-
     if method == 'GBN':
-        gbn(conn, addr, data, file)
+        print('Sending the file using Go-Back-N')
+        thread = threading.Thread(target=gbn, args=(conn, addr))
+        thread.start()
     elif method == 'SR':
-        sr(conn)
-
+        print('Sending the file using Selective Repeat')
+        thread = threading.Thread(target=sr, args=(conn))
+        thread.start()
